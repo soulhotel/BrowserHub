9e6d752f7cea279d060f60f921282cd7a19f6bb8
soulhotel <155501797+soulhotel@users.noreply.github.com>
2024-03-03 09:19:16 -0500

    Delete background.js

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.action === 'openDevToolsInNewTab') {
-    chrome.tabs.create({ url: 'chrome-devtools://devtools/bundled/inspector.html' });
-  }
-});
